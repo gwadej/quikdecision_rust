598349e10df6b483b7c00f5424b43dfcf2140937
G. Wade Johnson <gwadej@anomaly.org>
2018-10-30 22:53:18 -0500

    Supply a help screen

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ mod oracle;
 
 pub fn parse_args(mut args: std::env::Args) -> Result<Command, String>
 {
-    args.next();  // discard program name
+    let progname = args.next().unwrap();
     let cmd = match args.next()
     {
         Some(c)  => c,
@@ -27,6 +27,7 @@ pub fn parse_args(mut args: std::env::Args) -> Result<Command, String>
         "roll"    | "dice"   => dice::command(&mut args),
         "select"             => select::command(&mut args),
         "oracle"             => oracle::command(),
+        "help"               => usage(progname),
         _                    => Err(String::from("Unknown command")),
     }
 }
@@ -58,6 +59,35 @@ impl Decider for Command {
             Command::Oracle               => oracle::spake(),
         }
     }
+}
+
+fn usage(progname: String) -> !
+{
+    println!("{} {}\n", progname, "{command} [cmd_args ...]");
+    println!("{}\n", "where {command} is one of:");
+    println!("{}", "  flip    - 50% chance of a Heads or Tails");
+    println!("{}", "  coin    - alias for flip");
+    println!("{}", "  pick {low} {high}");
+    println!("{}", "          - pick a number between {low} and {high}");
+    println!("{}", "  choose  - alias for pick");
+    println!("{}", "  percent {num}");
+    println!("{}", "          - True {num} percent of the time, otherwise False");
+    println!("{}", "  likely  - alias for percent");
+    println!("{}", "  roll {dice expr}");
+    println!("{}", "          - Roll the described combination of dice, returning a number");
+    println!("{}", "            {dice expr} is a combination of terms of the form {n}d{s}");
+    println!("{}", "            where {n} is a positive integer, {s} is a number of sides");
+    println!("{}", "            for the dice (4, 6, 8, 10, 12, 20, or 100). A term can also");
+    println!("{}", "            be just an integer. (e.g. 2d6+1d4+2)");
+    println!("{}", "  dice    - alias for roll");
+    println!("{}", "  select {strs}");
+    println!("{}", "          - Select one of two or more string supplied as arguments");
+    println!("{}", "  select @{filename}");
+    println!("{}", "          - Select one of the lines in the file specified");
+    println!("{}", "  oracle  - Return a random answer from the oracle");
+    println!("{}", "  help    - This screen");
+
+    std::process::exit(1);
 }
 
 pub fn int_arg(args: &mut env::Args) -> Result<u32, String>
