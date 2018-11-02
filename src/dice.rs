8b03258cfae2ae789930180b117e7badade273ae
G. Wade Johnson <gwadej@anomaly.org>
2018-11-01 19:35:35 -0500

    Add individual dice roll information to display.

--- src/dice.rs
+++ src/dice.rs
@@ -95,28 +95,67 @@ pub fn command(args: &mut env::Args) -> Result<Command, String>
     Ok(Command::RollDice(descr))
 }
 
-fn roll_step(num: u32, sides: u32) -> u32
+fn roll_die(rng: &mut rand::ThreadRng, sides: u32) -> (String, u32)
 {
-    let mut rng = rand::thread_rng();
-    (1..=num)
-        .map(|_| rng.gen_range(1, sides+1))
-        .sum::<u32>()
+    let n = rng.gen_range(1, sides+1);
+    (n.to_string(), n)
 }
 
-fn roll_explode_step(num: u32, sides: u32) -> u32
+fn accum_roll(acc: (String, u32), roll: (String, u32), sep: &str) -> (String, u32)
 {
-    let mut rng = rand::thread_rng();
-    (1..=num)
-        .map(|_| rng.gen_range(1, sides+1))
-        .map(|n| explode(n, sides))
-        .sum::<u32>()
-}
-
-fn explode(val: u32, sides: u32) -> u32
-{
-    if val == sides
+    if acc.0.len() == 0
     {
-        sides+roll_explode_step(1, sides)
+        (roll.0, acc.1+roll.1)
+    }
+    else
+    {
+//        (format!("{}{}{}", acc.0, sep, roll.0), acc.1+roll.1)
+        (acc.0 + sep + &roll.0, acc.1+roll.1)
+    }
+}
+
+fn roll_step(num: u32, sides: u32) -> (String, u32)
+{
+    let mut rng = rand::thread_rng();
+    let out = (1..=num)
+        .map(|_| roll_die(&mut rng, sides))
+        .fold((String::new(), 0), |acc, r| accum_roll(acc, r, "+"));
+    (format!("{}d{}({})", num, sides, out.0), out.1)
+}
+
+fn roll_explode_step(num: u32, sides: u32) -> (String, u32)
+{
+    let mut rng = rand::thread_rng();
+    let out = (1..=num)
+        .map(|_| roll_die(&mut rng, sides))
+        .map(|r| explode(r, sides))
+        .fold((String::new(), 0), |acc, r| accum_roll(acc, r, "+"));
+    (format!("{}x{}[{}]", num, sides, trim(out.0)), out.1)
+}
+
+fn trim(instr: String) -> String
+{
+    String::from(instr.trim_start().trim_end())
+}
+
+fn roll_exploded_step(sides: u32) -> (String, u32)
+{
+    let mut rng = rand::thread_rng();
+    let r = roll_die(&mut rng, sides);
+    explode(r, sides)
+}
+
+fn incr_step(num: u32) -> (String, u32)
+{
+    (num.to_string(), num)
+}
+
+fn explode(val: (String, u32), sides: u32) -> (String, u32)
+{
+    if val.1 == sides
+    {
+        let roll = roll_exploded_step(sides);
+        (format!(" ({}!+{}) ",val.0, roll.0), val.1+roll.1)
     }
     else
     {
@@ -126,14 +165,14 @@ fn explode(val: u32, sides: u32) -> u32
 
 pub fn roll(descr: Vec<Roll>) -> String
 {
-    let value = descr.iter()
+    let val = descr.iter()
         .map(|ref x| match x
              {
                  Roll::Dice(num,sides)          => roll_step(*num, *sides),
                  Roll::ExplodingDice(num,sides) => roll_explode_step(*num, *sides),
-                 Roll::Incr(num)                => *num,
+                 Roll::Incr(num)                => incr_step(*num),
              })
-        .sum::<u32>();
-    value.to_string()
+        .fold((String::new(), 0), |acc, r| accum_roll(acc, r, " + "));
+        format!("{}: {}", val.1, val.0)
 }
 
