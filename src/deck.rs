e01a05b7e83fa19f7985ceb8b6b644f051608964
G. Wade Johnson <gwadej@anomaly.org>
2019-01-13 15:17:21 -0600

    Both normal decks work

--- src/deck.rs
+++ src/deck.rs
@@ -5,19 +5,18 @@ use crate::ApiDoc;
 use rand::Rng;
 
 #[derive(Debug)]
-pub struct Card
+pub enum Card
 {
-    pub glyph: char,
-    suit: &'static str,
-    number: usize,
+    Pip{glyph: char, suit: &'static str, number: usize},
+    Face{glyph: char, suit: &'static str, face: &'static str},
+    Joker{glyph: char, name: &'static str},
 }
 
 #[derive(Debug)]
 pub enum Deck
 {
     Standard52,
-    Joker,
-    TwoJokers,
+    Jokers,
     Tarot,
 }
 
@@ -28,50 +27,12 @@ impl PartialEq for Deck
         match (self, other)
         {
             (Deck::Standard52, Deck::Standard52) => true,
-            (Deck::Joker, Deck::Joker)           => true,
-            (Deck::TwoJokers, Deck::TwoJokers)   => true,
-            (Deck::Tarot, Deck::Tarot)           => true,
+            (Deck::Jokers,     Deck::Jokers)     => true,
+            (Deck::Tarot,      Deck::Tarot)      => true,
             (_,_)                                => false,
         }
     }
 }
-
-//#[derive(Debug)]
-//pub enum Card
-//{
-//    Wild(&'static str),
-//    Ace(&'static str),
-//    Pip{suit: &'static str, value: u32},
-//    Court{suit: &'static str, face: &'static str},
-//    Trump{name: &'static str, value: u32},
-//}
-//
-//impl Card
-//{
-//    pub fn to_string(&self) -> String
-//    {
-//        match self
-//        {
-//            Card::Wild(name) => name.to_string(),
-//            Card::Ace(suit)  => format!("Ace of {}", suit),
-//            Card::Pip{ suit, value } => format!("{} of {}", value, suit),
-//            Card::Court{ suit, face } => format!("{} of {}", face, suit),
-//            Card::Trump{ name, value } => format!("{} ({})", name, value),
-//        }
-//    }
-//
-//    pub fn short_name(&self) -> String
-//    {
-//        match self
-//        {
-//            Card::Wild(name) => name.to_string(),
-//            Card::Ace(suit)  => format!("A{}", suit.get(..1).unwrap()),
-//            Card::Pip{ suit, value } => format!("{}{}", value, suit.get(..1).unwrap()),
-//            Card::Court{ suit, face } => format!("{}{}", face, suit.get(..1).unwrap()),
-//            Card::Trump{ name:_, value } => format!("Tr{}", value),
-//        }
-//    }
-//}
 
 const SUITS: [&str; 4] = [
     "Spades",
@@ -80,7 +41,7 @@ const SUITS: [&str; 4] = [
     "Clubs",
 ];
 const FACES: [&'static str; 3] = [ "Jack", "Queen", "King" ];
-//const JOKERS: [&'static str; 3] = [ "Black Joker", "Red Joker", "White Joker" ];
+const JOKERS: [&'static str; 3] = [ "Black Joker", "Red Joker", "White Joker" ];
 const GLYPHS: &str =
     "\u{1F0A1}\u{1F0A2}\u{1F0A3}\u{1F0A4}\u{1F0A5}\u{1F0A6}\u{1F0A7}\u{1F0A8}\u{1F0A9}\u{1F0AA}\u{1F0AB}\u{1F0AD}\u{1F0AE}\
      \u{1F0B1}\u{1F0B2}\u{1F0B3}\u{1F0B4}\u{1F0B5}\u{1F0B6}\u{1F0B7}\u{1F0B8}\u{1F0B9}\u{1F0BA}\u{1F0BB}\u{1F0BD}\u{1F0BE}\
@@ -90,16 +51,39 @@ const GLYPHS: &str =
 //     \u{1F0E0}\u{1F0E1}\u{1F0E2}\u{1F0E3}\u{1F0E4}\u{1F0E5}\u{1F0E6}\u{1F0E7}\u{1F0E8}\u{1F0E9}\u{1F0EA}\u{1F0EB}\u{1F0EC}\u{1F0ED}\u{1F0EE}\u{1F0EF}\u{1F0F0}\u{1F0F1}\u{1F0F2}\u{1F0F3}\u{1F0F4}\u{1F0F5}"
 ;
 
+impl Card
+{
+    pub fn glyph(&self) -> char
+    {
+        match self
+        {
+            Card::Pip{glyph, ..}   => *glyph,
+            Card::Face{glyph, ..}  => *glyph,
+            Card::Joker{glyph, ..} => *glyph,
+        }
+    }
+}
+
 impl std::string::ToString for Card
 {
     fn to_string(&self) -> String
     {
-        match self.number
+        match self
         {
-            1 => format!("Ace of {}", self.suit),
-            2...10 => format!("{} of {}", self.number, self.suit),
-            11...13 => format!("{} of {}", FACES[self.number-11], self.suit),
-            _ => String::new(),
+            Card::Pip{number, suit, ..} => {
+                match number
+                {
+                    1 => format!("Ace of {}", suit),
+                    2...10 => format!("{} of {}", number, suit),
+                    _ => String::new(),
+                }
+            },
+            Card::Face{suit, face, ..} => {
+                format!("{} of {}", face, suit)
+            },
+            Card::Joker{name, ..} => {
+                format!("{}", name)
+            },
         }
     }
 }
@@ -110,21 +94,51 @@ pub fn command(deck: &str) -> Result<Command, String>
     match deck
     {
         "52-card"  => Ok(Command::DrawCard(Deck::Standard52)),
-        "joker"    => Ok(Command::DrawCard(Deck::Joker)),
-        "2 jokers" => Ok(Command::DrawCard(Deck::TwoJokers)),
+        "jokers"   => Ok(Command::DrawCard(Deck::Jokers)),
         "tarot"    => Ok(Command::DrawCard(Deck::Tarot)),
-        _ => Err("Unrecognized deck type".to_string()),
+        _          => Err("Unrecognized deck type".to_string()),
+    }
+}
+
+fn get_glyph(num: usize) -> char
+{
+    GLYPHS.chars().nth(num).unwrap_or('\u{1F0A0}')
+}
+
+fn draw_standard(num: usize) -> Result<Card,String>
+{
+    let (suit, rank) = (num / 13, (num % 13) + 1);
+    match rank
+    {
+        1...10  => Ok(Card::Pip{ glyph: get_glyph(num), suit: SUITS[suit], number: rank }),
+        11...13 => Ok(Card::Face{ glyph: get_glyph(num), suit: SUITS[suit], face: FACES[rank-11] }),
+        _       => Err("Can't get here!".to_string()),
     }
 }
 
 /// Draw a card from the deck
-pub fn draw(_deck: Deck) -> Decision
+pub fn draw(deck: Deck) -> Decision
 {
-    let num = rand::thread_rng().gen_range(0, 52);
-    let (suit, rank) = (num / 13, (num % 13) + 1);
-    Decision::Card(
-        Card{ glyph: GLYPHS.chars().nth(num).unwrap(), suit: SUITS[suit], number: rank }
-    )
+    let card = match deck
+    {
+        Deck::Standard52 => {
+            let num = rand::thread_rng().gen_range(0, 52);
+            draw_standard(num).unwrap()
+        },
+        Deck::Jokers => {
+            let num = rand::thread_rng().gen_range(0, 54);
+            if num >= 52
+            {
+                Card::Joker{ glyph: get_glyph(num), name: JOKERS[num-52] }
+            }
+            else
+            {
+                draw_standard(num).unwrap()
+            }
+        },
+        _ => panic!("Not yet implemented!"),
+    };
+    Decision::Card(card)
 }
 
 /// Return an ApiDoc object containing a description of the DrawCard
